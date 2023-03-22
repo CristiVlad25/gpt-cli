91fafc95530bc14ede8e90b31c5e0020dcd243a4
Cristi <CristiVlad25@users.noreply.github.com>
2023-03-22 12:36:45 +0200

    Update gpt-text-davinci-003.sh

--- gpt-text-davinci-003.sh
+++ gpt-text-davinci-003.sh
@@ -6,7 +6,7 @@ export OPENAI_API_KEY="Your OPENAI API KEY"
 # Set the model to use (e.g. "text-davinci-003") - this is very capable and also inexpensive (in between 3.5-turbo and 4)
 MODEL="text-davinci-003"
 
-# Set the prompt to use as input for the GPT-3 model
+# Set the prompt to use as input for the language model
 PROMPT="$1"
 
 # Set the number of tokens to generate
@@ -24,7 +24,7 @@ FREQUENCY=0.5
 # Set the presence
 PRESENCE=0.5
 
-# Generate text using the GPT-3 model
+# Generate text using the language model
 OUTPUT=$(curl -sk -X POST -H "Authorization: Bearer $OPENAI_API_KEY" -H "Content-Type: application/json" -d "{\"model\":\"$MODEL\",\"prompt\":\"$PROMPT\",\"temperature\":$TEMPERATURE,\"top_p\":$TOP_P,\"frequency_penalty\":$FREQUENCY,\"presence_penalty\":$PRESENCE,\"max_tokens\":$TOKEN_COUNT}" https://api.openai.com/v1/completions)
 
 # Extract the text from the response
