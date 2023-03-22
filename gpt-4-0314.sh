ef88c70474dee6f171199c1b2725406e3ca8b35d
Cristi <CristiVlad25@users.noreply.github.com>
2023-03-22 12:35:15 +0200

    Create gpt-4-0314.sh

---
+++ gpt-4-0314.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set your OpenAI API key
+export OPENAI_API_KEY="Your OPENAI API KEY"
+
+# Set the model to use (e.g. "gpt-4-0314") - this is the most powerful and most expensive model
+MODEL="gpt-4-0314"
+
+# Set the prompt to use as input for the language model
+PROMPT="$1"
+
+# Set the number of tokens to generate
+TOKEN_COUNT=500
+
+# Set the temperature
+TEMPERATURE=0.0
+
+# Set the top_p value
+TOP_P=1
+
+# Set the frequency
+FREQUENCY=0.5
+
+# Set the presence
+PRESENCE=0.5
+
+# Generate text using the language model
+OUTPUT=$(curl -sk -X POST -H "Authorization: Bearer $OPENAI_API_KEY" -H "Content-Type: application/json" -d "{\"model\":\"$MODEL\",\"messages\":[{\"role\":\"user\",\"content\":\"$PROMPT\"}],\"temperature\":$TEMPERATURE,\"top_p\":$TOP_P,\"frequency_penalty\":$FREQUENCY,\"presence_penalty\":$PRESENCE,\"max_tokens\":$TOKEN_COUNT}" https://api.openai.com/v1/chat/completions)
+
+# Extract the text from the response
+OUTPUT=$(echo "$OUTPUT" | jq -r '.choices[0].message.content')
+
+# Print the output
+echo "$OUTPUT"
